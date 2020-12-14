da4d89e3960a53a092611402ebf68b38d68489fc
David Nemec <d4v1d.nemec@gmail.com>
2020-12-14 12:08:51 +0100

    Add application

---
+++ ConsoleApp/Application.cs
@@ -0,0 +1,54 @@
+﻿using Moravia.Homework.DocumentConverter;
+using Moravia.Homework.DocumentConverter.Abstractions;
+using StorageProvider.Abstractions;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Application
+    {
+        private readonly IConverter _converter;
+        private readonly IStorageProvider _storageProvider;
+
+        public Application(IConverter converter, IStorageProvider storageProvider)
+        {
+            _converter = converter;
+            _storageProvider = storageProvider;
+        }
+
+        public async Task Convert(Uri fromUri, Uri toUri)
+        {
+            var fromFileType = GetFileExtension(fromUri);
+            var toFileType = GetFileExtension(toUri);
+
+            var fromStorage = _storageProvider.GetStorage(fromUri.Scheme);
+            var toStorage = _storageProvider.GetStorage(toUri.Scheme);
+
+            if (!fromStorage.CanRead)
+                throw new Exception($"Cannot read from {fromStorage.StorageType} ({fromUri.AbsolutePath}).");
+            if (!toStorage.CanWrite)
+                throw new Exception($"Cannot write to {toStorage.StorageType} ({toUri.AbsolutePath}).");
+
+            using (var fromStream = await fromStorage.OpenReadAsync(fromUri))
+            using (var toStream = await toStorage.OpenWriteAsync(toUri))
+            {
+                await _converter.ConvertAsync(fromStream, fromFileType, toStream, toFileType);
+            }
+        }
+
+        private FileType GetFileExtension(Uri uri)
+        {
+            string stringExtension = Path.GetExtension(uri.AbsolutePath)
+                ?? throw new ArgumentException("Provided URI does not contain extension", nameof(uri));
+
+            stringExtension = stringExtension[1..]; // Remove period.
+
+            if (!Enum.TryParse<FileType>(stringExtension, true, out var result))
+                throw new Exception($"File extension '{stringExtension}' not supported.");
+
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using Moravia.Homework.DocumentConverter.Abstractions;
+using Moravia.Homework.DocumentConverter.Converters;
+using Moravia.Homework.DocumentConverter.Models;
+using Moravia.Homework.DocumentConverter.Serializers;
+using StorageProvider.Abstractions;
+using StorageProvider.Storage;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Initialize dependencies.
+            var jsonSerializer = new JsonDocumentSerializer();
+            var xmlSerializer = new XmlDocumentSerializer();
+            var converter = new ConverterBase<Document>(new ISerializer<Document>[] { jsonSerializer, xmlSerializer, });
+
+            var webStorage = new WebStorage();
+            var fileSystemStorage = new FileSystemStorage();
+            var storageProvider = new StorageProvider.Storage.StorageProvider(new IStorage[] { webStorage, fileSystemStorage, });
+
+            var app = new Application(converter, storageProvider);
+
+            // Run application.
+            var fromUri = new Uri("https://gist.githubusercontent.com/daywee/155b9145f00967cffac2933869614d6c/raw/835a0feef86d671077a210b92e11f318abd0acb5/rwsDocument.json");
+
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var toUri = new Uri(Path.Combine(documentsPath, "rwsHomework.xml"));
+
+            await app.Convert(fromUri, toUri);
+        }
+    }
+}
