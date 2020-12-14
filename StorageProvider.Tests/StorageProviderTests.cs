4e46eafe3edc694fa95f7f5c22389af4ada0b4f1
David Nemec <d4v1d.nemec@gmail.com>
2020-12-14 11:52:09 +0100

    Add StorageProvider test

--- StorageProvider.Tests/StorageProviderTests.cs
+++ StorageProvider.Tests/StorageProviderTests.cs
@@ -1,10 +1,32 @@
+using Moq;
+using StorageProvider.Abstractions;
+using Xunit;
+
 namespace StorageProvider.Tests
 {
     public class StorageProviderTests
     {
-        public void StorageProviderReturnsCorrectStorageForGivenType()
+        [Theory]
+        [InlineData("http", StorageType.Http)]
+        [InlineData("https", StorageType.Http)]
+        [InlineData("file", StorageType.FileSystem)]
+        public void StorageProviderReturnsCorrectStorageForGivenType(string scheme, StorageType expectedStorageType)
         {
+            var webStorageMock = MockStorageWithType(StorageType.Http);
+            var fileStorageMock = MockStorageWithType(StorageType.FileSystem);
 
+            var provider = new Storage.StorageProvider(new IStorage[] { webStorageMock, fileStorageMock });
+
+            var actualStorageType = provider.GetStorage(scheme).StorageType;
+
+            Assert.Equal(expectedStorageType, actualStorageType);
+        }
+
+        private IStorage MockStorageWithType(StorageType type)
+        {
+            var mock = new Mock<IStorage>();
+            mock.Setup(e => e.StorageType).Returns(type);
+            return mock.Object;
         }
     }
 }
